1100dfd927fa5cf3c8a13eaa681372cfd25e0357
tezov <tezov.app@gmail.com>
2025-10-16 22:52:11 +0200

    add auth log

--- src/v1/auth.guard.ts
+++ src/v1/auth.guard.ts
@@ -14,13 +14,18 @@ export class AuthGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     const headers = request.headers;
-    const headerAuth = headers['authorization'] || headers['Authorization'];
+
+    console.log(headers)
+    console.log(this.loginTokenStore.getToken())
+
+    const headerAuth = headers['authorization'];
     const expected = this.loginTokenStore.getToken();
+
     if (!headerAuth || !expected) {
       throw new UnauthorizedException('missing or invalid authorization');
     }
 
-    const match = headerAuth.toString().trim() === `Bearer ${expected}`;
+    const match = headerAuth.toString().trim() === `bearer ${expected}`;
     if (!match) {
       throw new ForbiddenException('invalid token');
     }
