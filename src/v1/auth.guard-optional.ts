9b8d5d27980db41643bafb876b3818707636c465
tezov <tezov.app@gmail.com>
2025-12-06 20:52:22 +0100

    fix guard auth optional

--- src/v1/auth.guard-optional.ts
+++ src/v1/auth.guard-optional.ts
@@ -1,15 +1,29 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from './auth.guard';
+import { Reflector } from '@nestjs/core';
+import { PATH_METADATA } from '@nestjs/common/constants';
 
 @Injectable()
 export class AuthGuardOptional implements CanActivate {
-  constructor(private readonly authGuard: AuthGuard) {}
+  constructor(
+   private readonly reflector: Reflector,
+   readonly authGuard: AuthGuard
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
-    const headers = request.headers;
-    const headerAuth = headers['authorization'];
-    if (!headerAuth) return true;
-    return this.authGuard.canActivate(context);
+    const url: string = request.url || '';
+    const headerAuth = request.headers['authorization'];
+
+    const controllerPath = this.reflector.get<string>(
+      PATH_METADATA,
+      context.getClass(),
+    );
+    const base = `/${controllerPath}`;
+    const relative = request.url.startsWith(base) ? request.url.substring(base.length) : request.url;
+    if (url.startsWith('/auth') || headerAuth != null) {
+      return this.authGuard.canActivate(context);
+    }
+    return true;
   }
 }
